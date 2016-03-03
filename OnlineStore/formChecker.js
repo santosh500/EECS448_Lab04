79238da056f77828b8160487f94107a664843bb9
Paul Charles <santosh@ku.edu>
2016-03-03 00:55:49 -0600

    Second Commit for the Online Store

--- OnlineStore/formChecker.js
+++ OnlineStore/formChecker.js
@@ -6,10 +6,16 @@ function validate1()
    var night1 = document.getElementById('n').checked;
    var three1 = document.getElementById('t').checked;
    var a=0;
+   var regex1=/^([A-Za-z0-9_\-\.]){1,}\@([A-Za-z0-9_\-\.]){1,}\.([A-Za-z]{2,4})$/;
    if((passTag.length == 0))
    {
 	   alert("The Passwords are not there");
    	   return false;
+   }
+   else if(regex1.test(userTag)==false)
+   {
+   		alert("Please give username in username@domain.com format");
+   		return false;
    }
    else if(free1==false && night1==false && three1==false)
    {
